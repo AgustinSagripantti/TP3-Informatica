212634f4cd5b92598f293e1cfdd15c121bc1cd3e
Agustin <asagripantti@gmail.com>
2025-06-03 21:45:38 -0300

    Nuevo commit para tp 4

--- bmi.c
+++ bmi.c
@@ -15,7 +15,17 @@ int main()
     
     printf("Su imc es: %.2f", imc);
     
-    printf("\n\nÍndice | Condición\n------------------\n<18.5 | Bajo peso\n18.5 - 24.9 | Peso normal\n25 - 29.9 | Sobrepeso\n>30 | Obesidad");
+    printf("\n\nÍndice | Condición\n------------------\n<18.5 | Bajo peso\n18.5 - 24.9 | Peso normal\n25 - 29.9 | Sobrepeso\n>30 | Obesidad\n\n");
+    
+    if(imc < 18.5){
+        printf("Usted se encuentra en un bajo peso");
+    }else if(imc <= 24.9){
+        printf("Usted se encuentra en un peso normal");
+    }else if(imc <= 29.9){
+        printf("Usted se encuentra en sobrepeso");
+    }else{
+        printf("Usted se encuentra en un estado de obesidad");
+    }
     
     return 0;
 }
